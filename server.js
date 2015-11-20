fc0520a031a262973c92a120af6fe9bdb2554df9
Joseph J Easson <josephj.easson@gmail.com>
2015-11-20 10:41:24 +0000

    Add DELETE todos route

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/todos/:id', function(req, res){
 app.post('/todos',function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
 	
-	if (!_.isBoolean/(body.completed)|| !_.isString(body.description)|| body.description.trim().length === 0) {
+	if (!_.isBoolean(body.completed)|| !_.isString(body.description)|| body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
 	
@@ -49,6 +49,19 @@ app.post('/todos',function(req, res){
 	todos.push(body);
 		
 	res.json(body);
+	
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id,10); //
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 	
 });
 
