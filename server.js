8a3d7f999707d9b6f251824807f8e66141ecfb25
Joseph J Easson <josephj.easson@gmail.com>
2015-11-25 09:55:00 +0000

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,22 +16,28 @@ app.get('/', function(req, res){
 
 // GET /todos
 app.get('/todos', function(req, res){
-	var queryParams = req.query;
-	var filteredTodos = todos;
 	
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});	
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});	
+	var query = req.query;
+	var where = {};
+	
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	}else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 	
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length >0) {
-		filteredTodos = _.filter(filteredTodos, function(todo){
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
-	}npm 
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
+	}
 	
-	res.json(filteredTodos); // converts the array to json format	
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);	
+	}, function(e){
+		res.status(500).send();
+	});
+	
 });
 
 // GET /todos/:id
@@ -47,14 +53,6 @@ app.get('/todos/:id', function(req, res){
 	}, function(e) {
 		res.status(500).send();
 	});
-	
-	// var matchedTodo = _.findWhere(todos, {id: todoId}); // using the underscore module
-//
-// 	if (matchedTodo) {
-// 		res.json(matchedTodo);
-// 	} else {
-// 		res.status(404).send();
-// 	}
 });
 
 // POST /todos/
@@ -65,28 +63,7 @@ app.post('/todos',function(req, res){
 		res.json(todo.toJSON());
 	}, function(e){
 		res.status(400).json(e);
-	});
-	
-	// call create on db.todo
-	//	respond with 200 and todo
-	//	res.status(400).json(e)
-	
-	// if (!_.isBoolean(body.completed)|| !_.isString(body.description)|| body.description.trim().length === 0) {
-// 		return res.status(400).send();
-// 	}
-//
-// 	//set body.description to be trimmed value
-// 	body.description = body.description.trim();
-//
-// 	// add id field
-// 	body.id = todoNextId;
-// 	todoNextId++;
-//
-// 	// push body into array
-// 	todos.push(body);
-//
-// 	res.json(body);
-	
+	});	
 });
 
 // DELETE /todos/:id
